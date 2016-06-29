8d392fcb48f9d9238e3c30268b7cda3b8055f23f
Nguyen Le <nguyentle7@gmail.com>
2016-06-29 01:05:30 -0700

    working a bit

--- src/ts/server.ts
+++ src/ts/server.ts
@@ -1,18 +1,37 @@
 import {Server} from 'hapi'
+import * as Inert from 'inert'
 
 const server = new Server()
 server.connection({
-    host: 'localhost',
-    port: 8000
+    host: '0.0.0.0',
+    port: 8001
 })
 
-server.route({
-    method: 'GET',
-    path: '/hello',
-    handler: function (req, reply) {
-        return reply('hello world')
-    }
+server.register(Inert, (err) => {
+    if (err) { throw err }
 })
+
+server.route([
+    {
+        method: 'GET',
+        path: '/hello',
+        handler: function (req, reply) {
+            return reply('hello')
+        }
+    },
+    {
+        method: 'GET',
+        path: '/hello_world',
+        handler: function (req, reply) {
+            return reply('hello world2')
+        }
+    },
+    {
+        method: 'GET',
+        path: '/template',
+        handler: { file: './index.html' }
+    }
+])
 
 server.start((err) => {
     if (err) { throw err }
--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="globals/hapi/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+
+declare module "inert" {
+    var Inert : any
+    export {Inert}
+}
